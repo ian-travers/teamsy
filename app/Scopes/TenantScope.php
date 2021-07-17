6e805b5d7292b873d49d127a09c3ebd21cfd1a9f
Ian Travers <mr.itpe@gmail.com>
2021-07-17 17:42:41 +0300

    Use session to store tenant_id after login event

--- app/Scopes/TenantScope.php
+++ app/Scopes/TenantScope.php
@@ -17,6 +17,8 @@ class TenantScope implements Scope
      */
     public function apply(Builder $builder, Model $model)
     {
-        $builder->where('tenant_id', 1);
+        if (session()->has('tenant_id')) {
+            $builder->where('tenant_id', session()->get('tenant_id'));
+        }
     }
 }
