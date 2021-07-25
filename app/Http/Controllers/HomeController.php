8abf1460cf0455e65e8408605fe2171dedadcb91
Ian Travers <mr.itpe@gmail.com>
2021-07-25 17:55:27 +0300

    Super admin dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Login;
+use App\Models\Tenant;
+use App\Models\User;
+
 class HomeController extends Controller
 {
     public function show()
     {
         if(auth()->check()) {
-            return view('dashboard');
+            if(session()->has('tenant_id')) {
+                return view('dashboard');
+            }
+
+            return view('super.dashboard', [
+                'subscribersCount' => Tenant::count(),
+                'usersCount' => User::count(),
+                'loginsCount' => Login::count(),
+            ]);
         } else {
             return view('welcome');
         }
